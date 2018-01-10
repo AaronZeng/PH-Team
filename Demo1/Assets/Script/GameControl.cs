03ca3def359308f0f27c5b6617b12ed6b000eff5
AaronZeng <zr393362604@gmail.com>
2018-01-10 20:03:02 +0100

    Demo版本-12.12.2017
    上次开会时的版本

---
+++ Demo1/Assets/Script/GameControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class RendererExtensions
+{
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+}
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public bool gameOver = false;
+    public float scrollSpeed = -6f;
+
+    void Awake()
+    {
+        Input.multiTouchEnabled = true;
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        PlayerDied();
+
+        if (gameOver == true && Input.touchCount == 2)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void PlayerDied()
+    {
+        if (Player.rb2d.position.y < -7.5f || Player.rb2d.position.y > 7.5f 
+            || Player.rb2d.position.x < -9f || Player.rb2d.position.x > 9f)
+        {
+            gameOver = true;
+        }
+    }
+}
---
+++ Demo1/Assets/Script/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public float upForce = 400f;
+    public static Rigidbody2D rb2d;
+    public static bool changeGravity = false;
+    private float cd = 2f;
+    private float nextCd;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved && Time.time > nextCd)
+        {
+            nextCd= Time.time + cd;
+            changeGravity = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.touchCount == 1)
+        {
+                rb2d.velocity = Vector2.zero;
+                if (rb2d.gravityScale > 0)
+                {
+                    rb2d.AddForce(new Vector2(0, upForce));
+                }
+                else
+                {
+                    rb2d.AddForce(new Vector2(0, -upForce));
+                }
+        }
+    }
+}
