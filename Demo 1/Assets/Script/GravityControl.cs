10287dbb25124f94af39a86bcdc6e0f7d189ad5b
AaronZeng <zr393362604@gmail.com>
2018-01-10 13:53:45 +0100

    Demo 1
    编写中的Demo 1

---
+++ Demo 1/Assets/Script/GravityControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityControl : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+            rb2d.velocity = Vector2.zero;
+            rb2d.gravityScale *= -1;
+            sr.flipY = !sr.flipY;
+        }
+	}
+}
---
+++ Demo 1/Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public float upForce = 400f;
+
+    public static Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb2d.velocity = Vector2.zero;
+            if (rb2d.gravityScale > 0)
+            {
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+            else
+            {
+                rb2d.AddForce(new Vector2(0, -upForce));
+            }
+        }
+    }
+}
